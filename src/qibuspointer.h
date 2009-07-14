6d48ba56f78d7c87b2f507c32d5e2b7d071b0acb
Peng Huang <shawn.p.huang@gmail.com>
2009-07-14 17:39:58 +0800

    WIP.

---
+++ src/qibuspointer.h
@@ -0,0 +1,78 @@
+#ifndef __Q_IBUS_POINTER_H__
+#define __Q_IBUS_POINTER_H__
+
+#include <QAtomicInt>
+
+namespace IBus {
+
+template <typename T>
+class Pointer
+{
+public:
+    Pointer (T *object = 0) : p (0) {
+        set (object);
+    }
+
+    Pointer (const Pointer &src) : p (0) {
+        set (src.get ());
+    }
+
+    template <typename T1>
+    Pointer (const Pointer<T1> &src): p (0) {
+        set (src.get ());
+    }
+
+    ~Pointer () {
+        set (0);
+    }
+
+    Pointer &operator= (T *object) {
+        set (object);
+        return *this;
+    }
+    
+    Pointer &operator= (const Pointer &src) {
+        set (src.get ());
+        return *this;
+    }
+    
+    template <typename T1>
+    Pointer &operator= (const Pointer<T1> &src) {
+        set (src.get ());
+        return *this;
+    }
+
+    T &operator* () const {
+        return *get ();
+    }
+
+    T *operator-> () const {
+        return get ();
+    }
+
+    operator T*() const {
+        return get ();
+    }
+
+    T *get () const {
+        return p;
+    }
+
+    bool isNull () const {
+        return p == 0;
+    }
+
+protected:
+    T *p;
+
+    void set (T *object) {
+        if (p)
+            p->unref ();
+        if (object)
+            object->ref ();
+       p = object;
+    }
+};
+
+};
+#endif
