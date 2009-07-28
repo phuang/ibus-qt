7c98efdbf1a6a8dfe8b22310b1e63c7fb01016e1
Xingshen Luo <luoxsbupt@gmail.com>
2009-07-28 14:21:25 +0800

    modify for qibuslookuptable.cpp

--- src/qibuslookuptable.cpp
+++ src/qibuslookuptable.cpp
@@ -9,19 +9,21 @@ bool LookupTable::serialize(QDBusArgument & argument) const
     if ( !Serializable::serialize(argument) )
         return false;
 
-    argument.beginArray(QDBusArgument::VariantType);
-
     // read variables of basic type(non-container).
     argument << m_pagesize;
     argument << m_cursorPos;
     argument << m_cursorVisible;
     argument << m_round;
 
+    // read variables of container type
+    argument.beginArray(QDBusArgument::VariantType);
     for ( int i=0; i < m_candidates.size(); ++i )
     {
         argument << m_candidates[i];
     }
+    argument.endArray();
 
+    argument.beginArray(QDBusArgument::VariantType);
     for ( int i=0; i < m_lable.size(); ++i )
     {
         argument << m_lable[i];
@@ -36,20 +38,21 @@ bool LookupTable::deserialize (const QDBusArgument &argument)
     if ( !Serializable::deserialize(argument) )
         return false;
 
-    argument.beginArray();
-
     argument >> m_pagesize;
     argument >> m_cursorPos;
     argument >> m_cursorVisible;
     argument >> m_round;
 
+    argument.beginArray();
     while ( !argument.atEnd() );
     {
         TextPointer tp;
         argument >> tp;
         m_candidates.append(tp);
     }
+    argument.endArray();
 
+    argument.beginArray();
     while ( !argument.atEnd() );
     {
         TextPointer tp;
