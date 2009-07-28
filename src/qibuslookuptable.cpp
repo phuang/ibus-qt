1d66978d3e89357e2aeb610e9b73f7a6e8c76afe
Xingshen Luo <luoxsbupt@gmail.com>
2009-07-28 11:32:07 +0800

    modify for qibuslookuptable.cpp

--- src/qibuslookuptable.cpp
+++ src/qibuslookuptable.cpp
@@ -4,22 +4,38 @@ namespace IBus {
 
 IBUS_DECLARE_SERIALIZABLE(LookupTable, IBusLookupTable);
 
-bool
-LookupTable::serialize (QDBusArgument &argument) const
+bool LookupTable::serialize(QDBusArgument & argument) const
 {
-    if (!Serializable::serialize (argument))
-        return false;
-    argument << m_text;
+    argument.beginStructure();
+
+    // read variables of basic type(non-container).
+    argument << m_pagesize;
+    argument << m_cursorPos;
+    argument << m_cursorVisible;
+    argument << m_round;
+
+    // read container variables
+    argument.beginArray(QDBusArgument::VariantType);
+
+    for ( int i=0; i < m_candidates->size(); ++i )
+    {
+        argument << m_candidates[i];
+    }
+
+    for ( int i=0; i < m_table->size(); ++i )
+    {
+        argument << m_table[i];
+    }
+
+    argument.endArray();
+    argument.endStructure();
+
     return true;
 }
 
-bool
-LookupTable::deserialize (const QDBusArgument &argument)
-{
-    if (!Serializable::deserialize (argument))
-        return false;
-    argument >> m_text;
-    return true;
-}
+/*
+bool LookupTable::deserialize (const QDBusArgument &argument)
+{}
+*/
 
 };
--- src/qibuslookuptable.h
+++ src/qibuslookuptable.h
@@ -8,22 +8,44 @@ namespace IBus {
 class LookupTable;
 typedef Pointer<LookupTable> LookupTablePointer;
 
-class LookupTable : public Serializable
+class LookupTable: public Serializable
 {
     Q_OBJECT;
 
-protected:
-
 public:
-    LookupTable () {}
-    LookupTable (const QString &text) : m_text (text) {}
+    LookupTable() {}
+    LookupTable(uint pagesize,
+                uint cursorPos,
+                bool cursorVisible,
+                bool round,
+                QVector<QString> * candidates,
+                QVector<QString> * table)
+    {
+        m_pagesize = pagesize;
+        m_cursorPos = cursorPos;
+        m_cursorVisible = cursorVisible;
+        m_round = round;
+        m_candidates = candidates;
+        m_table = table;
+    }
 
-public:
-    virtual bool serialize (QDBusArgument &argument) const;
-    virtual bool deserialize (const QDBusArgument &argument);
+    ~LookupTable() {}
 
 private:
-    QString m_text;
+    virtual bool serialize(QDBusArgument & argument) const;
+    // virtual bool deserialize(const QDBusArgument &argument);
+
+private:
+    uint        m_pagesize;
+    uint        m_cursorPos;
+    bool        m_cursorVisible;
+    bool        m_round;
+    QVector<TextPointer>   m_candidates;
+    QVector<TextPointer>   m_table;
+    /*
+    QVector<LookupTablePointer>   *   m_candidates;
+    QVector<LookupTablePointer>   *   m_table;
+    */
 
     IBUS_SERIALIZABLE
 };
