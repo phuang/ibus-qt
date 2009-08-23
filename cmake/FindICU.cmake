9d7ebee41a9add12abba0bd9778317ccd93ed765
Peng Huang <shawn.p.huang@gmail.com>
2009-08-23 18:05:40 +0800

    Set ICU_LIBRARY_DIRS

--- cmake/FindICU.cmake
+++ cmake/FindICU.cmake
@@ -11,19 +11,28 @@ else(ICU_CONFIG_EXECUTABLE)
 endif(ICU_CONFIG_EXECUTABLE)
 
 if(ICU_FOUND)
+  # get include dirs
+  exec_program("${ICU_CONFIG_EXECUTABLE} --cppflags-searchpath|sed s/^-I//"
+    OUTPUT_VARIABLE ICU_INCLUDE_DIRS
+    RETURN_VALUE ERROR_CODE
+  )
+  separate_arguments(ICU_INCLUDE_DIRS)
+
+  # get libraries
+  exec_program("${ICU_CONFIG_EXECUTABLE} --ldflags-searchpath"
+    OUTPUT_VARIABLE ICU_LIBRARY_DIRS
+    RETURN_VALUE ERROR_CODE
+  )
+  separate_arguments(ICU_LIBRARY_DIRS)
+
   exec_program("${ICU_CONFIG_EXECUTABLE} --ldflags-libsonly"
     OUTPUT_VARIABLE ICU_LIBRARIES
     RETURN_VALUE ERROR_CODE
   )
   separate_arguments(ICU_LIBRARIES)
 
-  exec_program("${ICU_CONFIG_EXECUTABLE} --cppflags-searchpath|sed s/^-I//"
-    OUTPUT_VARIABLE ICU_INCLUDE_DIRS
-    RETURN_VALUE ERROR_CODE
-  )
-  separate_arguments(ICU_INCLUDE_DIRS)
 endif(ICU_FOUND)
 
 include(FindPackageHandleStandardArgs)
 find_package_handle_standard_args(ICU DEFAULT_MSG
-  ICU_LIBRARIES ICU_INCLUDE_DIRS)
+  ICU_LIBRARIES ICU_LIBRARY_DIRS ICU_INCLUDE_DIRS)
