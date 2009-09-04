6d460e1882c94abd5b88c0afe1fa974233d2f8ef
Peng Huang <shawn.p.huang@gmail.com>
2009-09-04 09:25:56 +0800

    Add demo engine.

---
+++ test/main.cpp
@@ -0,0 +1,46 @@
+#include <QCoreApplication>
+#include "qibusdemoengine.h"
+ 
+using namespace IBus;
+ 
+int main(int argc, char **argv)
+{
+    QCoreApplication app (argc, argv);
+ 
+    ComponentPointer cmpt = new Component("org.freedesktop.IBus.Qt.DemoEngine",
+                    "Qt Demo Engine",
+                    "0.1.0",
+                    "GPL",
+                    "Peng Huang <shawn.p.huang@gmail.com>",
+                    "http://code.google.com/p/ibus/",
+                    "",
+                    "ibus-hangul");
+ 
+    EngineDescPointer engine = new EngineDesc("hangul",
+                    "Qt Demo Input Method",
+                    "Qt Demo Input Method",
+                    "en",
+                    "GPL",
+                    "Peng Huang <shawn.p.huang@gmail.com>",
+                    "ibus",
+                    "us");
+    cmpt->addEngine(engine);
+ 
+    Bus bus;
+    if ( !bus.isConnected() ) {
+        qDebug () << "bus is not connected!";
+        return -1;
+    }
+ 
+    EngineFactory factory(bus.getConnection());
+    factory.addMetaObject("hangul", &(DemoEngine::staticMetaObject));
+
+    bus.registerObject ("/org/freedesktop/IBus", &factory);
+ 
+    if ( !bus.registerComponent(cmpt) ) {
+        qDebug () << "registerComponent error!";
+        return -1;
+    }
+    
+    return QCoreApplication::exec ();
+}
