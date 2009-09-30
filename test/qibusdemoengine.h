69a2f2594bca88b6ca3636b1a37f965e7312943a
Xingshen Luo <luoxsbupt@gmail.com>
2009-09-30 12:35:16 +0800

    update for demoengine

--- test/qibusdemoengine.h
+++ test/qibusdemoengine.h
@@ -1,7 +1,6 @@
 #ifndef __Q_IBUS_HANGUL_ENGINE_H_
 #define __Q_IBUS_HANGUL_ENGINE_H_
  
-#include <hangul-1.0/hangul.h>
 #include <qibus.h>
  
 using namespace IBus;
@@ -12,19 +11,7 @@ class DemoEngine : public Engine
  
 public :
     Q_INVOKABLE DemoEngine (const QString &name);
- 
     virtual ~DemoEngine ();
- 
-public :
-    void initialize ();
-    void clearup ();
-
-private :
-    // send signals
-    void UpdatePreeditText ();
-    void UpdateAuxiliaryText ();
-    void UpdateLookupTable ();
-    void CommitCurrentCandidate ();
 
 private :
     void propertyActivate (const QString &prop_name, int prop_state);
@@ -41,6 +28,7 @@ private :
     LookupTablePointer  m_lookupTable;
     PropertyPointer     m_prop;
     PropListPointer     m_propList;
+    TextPointer         m_candicate;
 };
  
 #endif
